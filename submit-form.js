f9a537e02b850c5d764ebf054a47634e30810b83
Developer-in-Tech <princeakinola05@gmail.com>
2023-01-06 15:59:04 +0100

    update

--- submit-form.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async function(event, context) {
-  const body = JSON.parse(event.body);
-  console.log(body);
-  return {
-    statusCode: 200,
-    body: 'Form data received'
-  };
-};
